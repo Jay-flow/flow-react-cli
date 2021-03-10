4b4a7ee13b63a5f77c9f4f2980ac63da0b856882
Jay-flow <kimpetro153@gmail.com>
2021-03-10 20:29:50 +0900

    [Enhance] Changed update commands hint

--- bin/update.ts
+++ bin/update.ts
@@ -5,15 +5,19 @@ import ora from "ora"
 const update = () => {
   program
     .command("update")
-    .description("Generate page(screen) component.")
+    .description("Update packages.")
     .action(() => {
       const updateCheckSpinner = ora("Check package updates...\n").start()
-      shell.exec("npx npm-check-updates --packageFile package.json -u")
-      updateCheckSpinner.succeed()
-      const npmInstallSpinner = ora("npm install...just a moment, please.\n").start()
-      shell.exec("npm install")
-      npmInstallSpinner.succeed()
-      process.exit(0)
+      try {
+        shell.exec("npx npm-check-updates --packageFile package.json -u")
+        updateCheckSpinner.succeed()
+        const npmInstallSpinner = ora("npm install...just a moment, please.\n").start()
+        shell.exec("npm install")
+        npmInstallSpinner.succeed()
+        process.exit(0)
+      } catch (e) {
+        updateCheckSpinner.fail()
+      }
     })
 }
 
